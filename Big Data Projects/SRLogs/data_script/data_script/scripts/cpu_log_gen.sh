9ce0d4836236c1a1338a8ab3570db6c258e0a42a
root <pandeyabhishek961@gmail.com>
2019-10-02 22:17:28 +0530

    Message

--- Big Data Projects/SRLogs/data_script/data_script/scripts/cpu_log_gen.sh
+++
@@ -1,1 +0,0 @@
-echo "`hostname` `date +%d-%m-%y,%H:%M` ` sar 1 60 |tail -1 `" >> /home/hadoop/logs/`date +%m-%y`-cpu-sar.txt &
--- Big Data Projects/SRLogs/data_script/data_script/scripts/memory_log_gen.sh
+++
@@ -1,1 +0,0 @@
-echo "`hostname` `date +%d%m%y,%H:%M` ` sar -r 1 59 |tail -1 `" >> /home/hadoop/logs/`date +%m-%y`-memory-sar.txt &
